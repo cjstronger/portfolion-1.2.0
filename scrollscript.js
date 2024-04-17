c1e6cbc8022c75d0e438537f84e0103f14409890
Clint Strong <clintjstrong@gmail.com>
2024-04-16 23:51:18 -0700

    Timings and loading animation
    Added the stussy animation, changed a load of timings as the page wasnt quite ready for the timing of the animation. Also deleted the magnetic effect the old buttons on the top of the page had.

--- scrollscript.js
+++ scrollscript.js
@@ -15,7 +15,7 @@ let tl = gsap.timeline({
     start: 'top center',
     end: '40% center',
     scrub: true,
-    markers: true
+    markers: false
   }
 })
 
@@ -34,10 +34,22 @@ window.addEventListener('load', function(){
     RLoader.classList.add('loading-SlideR')
     TLoader.classList.add('loading-SlideT')
     BLoader.classList.add('loading-SlideB')
-  }, 2500)
+  }, 3500)
 })
 
+let stussyMove = document.getElementById('stussy')
+setTimeout(function() {
+  stussyMove.classList.add('stussy-move')
+}, 4200)
 // Loading the lottie files
+
+var stussyAnimation = bodymovin.loadAnimation({
+  container: document.getElementById("stussy"),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'stussy.json'
+})
 
 window.addEventListener('load', function(){
     var firstAnimation = bodymovin.loadAnimation({
@@ -57,36 +69,34 @@ window.addEventListener('load', function(){
     setTimeout(() => {
       lastAnimation.play()
       firstAnimation.play()
-    }, 3500)
+    }, 4500)
 })
 
 let buttons = document.getElementById('navbar')
 window.addEventListener('load', function(){
   this.setTimeout(function(){
     buttons.classList.add('nav-glide')
-  }, 5500)
+  }, 7500)
 })
 
 let army = document.getElementById('army')
 window.addEventListener('load', function(){
   setTimeout(function(){
     army.classList.add('opacity')
-  }, 5000)
+  }, 6000)
 })
 
 const observerOptions = {
-  root: null, // null means the viewport
-  rootMargin: '0px', // Adjust margins to change the effective area
-  threshold: 1.0 // Callback is triggered when 50% of the target is visible
+  root: null,
+  rootMargin: '0px', 
+  threshold: 1.0
 };
 
 const observerCallback = (entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      // Target element is intersecting with the root's threshold
-      entry.target.classList.add('show-animate');
-    } else {
-      // Optional: Do something when the element is not intersecting
+        entry.target.classList.add('show-animate');
+      } else {
     }
   });
 };
@@ -99,17 +109,6 @@ document.querySelectorAll('.hidden').forEach(hidden => {
   observer.observe(hidden);
 });
 
-let btn = document.querySelectorAll('.button').forEach(btn => {
-  btn.addEventListener('mousemove',(e)=>{
-    let pos = btn.getBoundingClientRect();
-    let x = e.pageX - pos.left - pos.width / 2;
-    let y = e.pageY - pos.top - pos.height / 2 -window.scrollY;
-    btn.children[0].style.transform = "translate(" + x *.3 + "px, " + y *.5 + "px)";
-    btn.addEventListener('mouseout',(e)=>{
-      btn.children[0].style.transform = "translate(0px, 0px)";
-    })
-  })
-})
 
 //Scroll modification to enable the nav bar to disappear when scrolling down and appear
 //when scrolling up.
@@ -131,13 +130,13 @@ window.addEventListener('scroll', ()=>{
 let dhaTimeout = {
   id: null,
   startTime: 0,
-  remainingTime: 10000,
+  remainingTime: 11000,
   isPaused: false
 }
 let stackTimeout = {
   id: null,
   startTime: 0,
-  remainingTime: 15000,
+  remainingTime: 16000,
   isPaused: false
 }
 

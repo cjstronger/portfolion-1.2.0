835aac6fbd3a2f87f0f7401d3f6419439a24ac43
cjstronger <clintjstrong@gmail.com>
2024-10-12 19:50:37 -0700

    changed files

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const projectsRouter = require("./routes/projectsRouter");
+const homeRouter = require("./routes/homeRouter");
+const path = require("path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", homeRouter);
+app.use("/projects", projectsRouter);
+
+app.listen(process.env.port, () => {
+  console.log("Listening on port 3000");
+});
+
+module.exports = app;
